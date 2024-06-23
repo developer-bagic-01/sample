dbdff9cefa545bc3185f3411a8ccdc7dcb1b3d78
developer-1 <keytanthom@gmail.com>
2024-06-23 22:58:29 +0530

    added new

--- src/main/java/com/demo/service/ServiceCities.java
+++ src/main/java/com/demo/service/ServiceCities.java
@@ -3,7 +3,7 @@ package com.demo.service;
 public class ServiceCities {
 
 	public String cities () {
-		return "mumbai,pune,banglore,hyderabad,demo-city,supe,tupe,diva,ladds,lafrv";
+		return "mumbai,pune,banglore,hyderabad,demo-city,supe,tupe,diva,ladds,lafrv,gdsf";
 	}
 
 	
